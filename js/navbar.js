2abfd044dcc5189d2d8cc0f002c95befd6ae058f
Antoine Elissagaray <antoine.elissagaray@orange.fr>
2023-05-22 17:23:09 +0200

    maj

---
+++ js/navbar.js
@@ -0,0 +1,7 @@
+/* ================= NAVBAR ======================*/
+const menuHamburger = document.querySelector(".menu-hamburger")
+const navLinks = document.querySelector(".nav-links")
+
+menuHamburger.addEventListener('click',() => {
+navLinks.classList.toggle('mobile-menu')
+})
