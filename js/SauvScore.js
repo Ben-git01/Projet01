ed6cd9b9098f101f7a60d842bfe52f6c953115cb
Antoine Elissagaray <antoine.elissagaray@orange.fr>
2023-05-24 09:32:58 +0200

    commentaire partie sauvegarde score

--- js/SauvScore.js
+++ js/SauvScore.js
@@ -17,28 +17,17 @@ function sauvegarderPartie(nomJoueur, score, niveauDifficulte) {
 function chargerSauvegardes() {
   // Lire les sauvegardes précédentes
   fs.readFile('sauvegardes.txt', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      // Traiter les sauvegardes précédentes
-      const sauvegardes = data.split('\n');
-      sauvegardes.forEach((sauvegarde) => {
-        if (sauvegarde.trim() !== '') {
-          const elements = sauvegarde.split(',');
-          const nomJoueur = elements[0];
-          const score = parseInt(elements[1]);
-          const niveauDifficulte = parseInt(elements[2]);
-
-          // Faire quelque chose avec les données chargées (par exemple, les afficher)
-          console.log(`Nom du joueur : ${nomJoueur}, Score : ${score}, Niveau de difficulté : ${niveauDifficulte}`);
-        }
-      });
-    }
-  });
+    
+    //Code ici - gogogo 
+    
+  
+  })
 }
+
 
 // Exemple d'utilisation
 sauvegarderPartie('Alice', 100, 2);
 sauvegarderPartie('Bob', 75, 1);
+
 
 chargerSauvegardes();
